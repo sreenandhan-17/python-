22389a4d6406520eee7340e03939b97ecec7f5fa
sreenandhan-17 <sreenandhan-17@example.com>
2026-08-13 14:24:06 +0530

    Update conversionofstringtointegerorfloat.py and other changes

---
+++ conversionofstringtointegerorfloat.py
@@ -0,0 +1,8 @@
+int1='5'
+float1='3.6'
+converted_int=int(int1)
+converted_float=float(float1)
+print('the converted int:',converted_int)
+print('the converted float:',converted_float)
+print(type(converted_int))
+print(type(converted_float))
