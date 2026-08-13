5ffc3fdcefd05502ee914d3e2f3696abeb96aba7
sreenandhan-17 <sreenandhan-17@example.com>
2026-08-13 14:06:42 +0530

    Update exponentiation.py and other changes

---
+++ exponentiation.py
@@ -0,0 +1,11 @@
+int1=5
+int2=6
+
+float1=3.2
+float2=6.3
+
+exp_int=int1**int2
+exp_float=float1**float2
+
+print('the exponentition of integer:',exp_int)
+print('the exponentiation of float:',exp_float)
