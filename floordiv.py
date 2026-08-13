46ad0d414f4c4799891a9abd12df8e526f9ed5d3
sreenandhan-17 <sreenandhan-17@example.com>
2026-08-13 14:01:55 +0530

    Update floordiv.py and other changes

---
+++ floordiv.py
@@ -0,0 +1,11 @@
+int1=56
+int2=12
+
+float1=5.4
+float2=12.0
+
+floor_div_int=int1//int2
+floor_div_float=float2//float1
+
+print('the integer floor division:',floor_div_int)
+print('the float floor division:',floor_div_float)
